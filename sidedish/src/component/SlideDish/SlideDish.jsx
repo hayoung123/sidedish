e95542d324e940ff2430f7bd5dbb94bc4e32c199
DESKTOP-O9GP521\J <junzero741@gmail.com>
2021-04-22 22:38:58 +0900

    [Style]: slide arrow button conditional style

--- sidedish/src/component/SlideDish/SlideDish.jsx
+++ sidedish/src/component/SlideDish/SlideDish.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import DishItem from 'component/DishItem/DishItem';
 import { IoChevronBackSharp, IoChevronForwardSharp } from 'react-icons/io5';
 import { URL } from 'util/data';
@@ -27,11 +27,18 @@ const SlideDish = ({ category }) => {
     <SlideContainer>
       <Header>모두가 좋아하는 든든한 메인요리</Header>
       <SlideWrapper>
-        <IoChevronBackSharp className="leftArrow" onClick={handleLeftClick} />
+        <IoChevronBackSharp
+          className={!slidePos ? 'disable leftArrow' : 'leftArrow'}
+          onClick={handleLeftClick}
+        />
         <StyledSlideList>
           <Slide slidePos={slidePos}>{slideCategory}</Slide>
         </StyledSlideList>
-        <IoChevronForwardSharp className="rightArrow" onClick={handleRightClick} />
+        <IoChevronForwardSharp
+          className={slidePos <= -(itemSize * contentsCnt) ? 'disable rightArrow' : 'rightArrow'}
+          onClick={handleRightClick}
+          slidePos={slidePos}
+        />
       </SlideWrapper>
     </SlideContainer>
   );
@@ -41,6 +48,7 @@ export default SlideDish;
 
 const Slide = styled.div`
   display: flex;
+  transition: transform 0.5s;
   transform: ${(props) => `translateX(${props.slidePos}px)`};
 `;
 
@@ -55,20 +63,24 @@ const SlideWrapper = styled.div`
   display: flex;
   flex-direction: row;
 
+  .disable {
+    stroke: #bdbdbd;
+  }
+
   .leftArrow {
     position: absolute;
-    left: -2rem;
+    left: -2.5rem;
     top: 50%;
-    width: 18px;
-    height: 18px;
+    width: 2em;
+    height: 2em;
   }
 
   .rightArrow {
     position: absolute;
     left: 101%;
     top: 50%;
-    width: 18px;
-    height: 18px;
+    width: 2em;
+    height: 2em;
   }
 `;
 
